dc2c13709cfa397331916356a6fd566a42a6f25e
davidbegin <davidmichaelbe@gmail.com>
2017-07-03 12:04:17 -0700

    Add ssh security group to EC2 instance

--- main.tf
+++ main.tf
@@ -6,8 +6,22 @@ resource "aws_instance" "babys_first_ec2" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.ssh_access.id}"]
 
   tags {
     Name = "terraform-kitchentest"
   }
 }
+
+resource "aws_security_group" "ssh_access" {
+  name = "ssh-access"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
